7b4deecc4b23b047b3e66555290cdbf2840fe734
Mujo Didic <80248965+MujoDidic@users.noreply.github.com>
2023-05-22 11:49:16 +0200

    Projektdateien hinzufügen.

---
+++ WindowsFormsTestWithDialogWindow/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTestWithDialogWindow
+{
+    public partial class Form1 : Form
+    {
+        SmallWindow SW_New = new SmallWindow();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //close both windows
+            SW_New.ShowDialog();
+            Close();
+
+        }
+    }
+}
